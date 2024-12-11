ad05f0e815231f89ca1753c68e496f144cf00a5e
Jonathan Martinez H <jonathan.martinez@bigwavedata.mx>
2024-12-10 22:17:36 -0600

    Paginaciones finales

--- app/Livewire/User/UserShow.php
+++ app/Livewire/User/UserShow.php
@@ -5,13 +5,19 @@ namespace App\Livewire\User;
 use App\Models\User;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Livewire\WithPagination;
 
 #[Title('Ver usuario')]
 class UserShow extends Component
 {
+    use WithPagination;
+    
     public User $user;
+    
     public function render()
     {
-        return view('livewire.user.user-show');
+        $sales = $this->user->sales()->paginate(5);
+
+        return view('livewire.user.user-show',compact('sales'));
     }
 }
--- resources/views/livewire/user/user-show.blade.php
+++ resources/views/livewire/user/user-show.blade.php
@@ -1,69 +1,84 @@
-<x-card cardTitle="Detalles de usuario">
+<x-card cardTitle="Detalles usuario">
     <x-slot:cardTools>
-        <a href="{{ route('users') }}" class="btn btn-primary">
-            <i class="fas fa-arrow-circle-left"></i>
-            Regresar
-        </a>
-    </x-slot:cardTools>
-
+       <a href="{{route('users')}}" class="btn btn-primary">
+        <i class="fas fa-arrow-circle-left"></i> Regresar
+       </a>
+    </x-slot>
+    
     <div class="row">
         <div class="col-md-4">
+
             <div class="card card-primary card-outline">
                 <div class="card-body box-profile">
-
-                    <h2 class="profile-username text-center">{{$user->name}}</h2>
-
                     <div class="text-center">
-                        <x-image :item="$user" size="250"/>
+                        <x-image :item="$user" size="250" />
                     </div>
-
-                    <p class="text-muted text-center">{{$user->admin ? 'Administrador' : 'Vendedor'}}</p>
-
+                    <h2 class="profile-username text-center mb-2">{{$user->name}}</h2>
+                    <p class="text-muted text-center">
+                        {{$user->admin ? 'Administrador' : 'Vendedor'}}
+                    </p>
+                
                     <ul class="list-group mb-3">
                         <li class="list-group-item">
-                            <b>Email</b> 
-                            <a class="float-right">{{$user->email}}</a>
+                            <b>Email</b> <a class="float-right">{{$user->email}}</a>
                         </li>
                         <li class="list-group-item">
-                            <b>Estado</b> 
-                            <a class="float-right">{!!$user->activeLabel!!}</a>
+                            <b>Estado</b> <a class="float-right">{!!$user->activeLabel!!}</a>
                         </li>
                         <li class="list-group-item">
-                            <b>Creado</b> 
-                            <a class="float-right">{{$user->created_at}}</a>
+                            <b>Creado</b> <a class="float-right">{{$user->created_at}}</a>
                         </li>
                     </ul>
+                    
                 </div>
-
+            
             </div>
+            
         </div>
         <div class="col-md-8">
             <table class="table text-center">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Imagen</th>
-                        <th>Producto</th>
-                        <th>Precio de Venta</th>
-                        <th>Stock</th>
+                        <th>Total</th>
+                        <th>Productos</th>
+                        <th>Articulos</th>
+                        <th>...</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    {{--@foreach ($category->products as $product)
+                    @foreach ($sales as $sale)
                     <tr>
-                        <td>{{$product->id}}</td>
                         <td>
-                            <x-image :item="$product"/>
+                           <b>FV-{{$sale->id}}</b>
                         </td>
-                        <td>{{$product->name}}</td>
-                        <td>{!!$product->precio!!}</td>
-                        <td>{!!$product->stockLabel!!}</td>
-                    </tr>
-                    @endforeach--}}
+                        <td>
+                            {{money($sale->total)}}
+                        </td>
+                        <td>
+                            <span class="badge badge-pill badge-primary">
+                                {{$sale->items->count()}}
+                            </span>
+                        </td>
+                        <td>
+                            <span class="badge badge-pill badge-primary">
+                                {{$sale->items->sum('pivot.qty')}}
+                            </span>                            
+                        </td>
+                        <td>
+                            <a href="{{route('sales.show',$sale)}}" class="btn btn-primary btn-sm">
+                                Ver venta
+                            </a>
+                        </td>
+                    </tr>                        
+                    @endforeach
+
                     
                 </tbody>
             </table>
-        </div>
+            {{$sales->links()}}
+        </div>    
     </div>
 
-</x-card>
+ </x-card>
