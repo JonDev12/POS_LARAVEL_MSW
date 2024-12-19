f5b1b37bdf9e2cf49a4a632398c9de763583ed8b
Jonathan Martinez <L21ISC023@tamazunchale.tecnm.mx>
2024-12-18 23:57:01 -0600

    Componente Editar ventas

--- app/Livewire/Sale/SaleEdit.php
+++ app/Livewire/Sale/SaleEdit.php
@@ -2,16 +2,44 @@
 
 namespace App\Livewire\Sale;
 
+use App\Models\Cart;
 use App\Models\Sale;
+use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Computed;
+use Livewire\WithPagination;
 
 #[Title('Editar venta')]
 class SaleEdit extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $cant = 5;
+    public $totalRegistros = 0;
     public Sale $sale;
+    public $cart;
+
     public function render()
     {
-        return view('livewire.sale.sale-edit');
+        return view('livewire.sale.sale-edit', [
+            'totalArticulos' => Cart::totalArticulos(),
+            'total' => Cart::getTotal(),
+            'products' => $this->products(),
+        ]);
     }
-}
+
+    public function mount()
+    {
+        $this->cart = collect();
+    }
+
+     //Prppiedad para obtener los productos
+     #[Computed()]
+     public function products(){
+         return Product::where('name', 'like', '%'.$this->search.'%')
+             ->orderBy('id', 'desc')
+             ->paginate($this->cant);
+     }
+ }
--- resources/views/livewire/sale/sale-edit.blade.php
+++ resources/views/livewire/sale/sale-edit.blade.php
@@ -1,4 +1,29 @@
 <div>
-    {{-- Stop trying to control. --}}
-    @dump($sale)
+    <x-card cardTitle="Crear Venta">
+       <x-slot:cardTools>
+          <a href="{{route('sales.list')}}" class="btn btn-primary btn-sm mr-2" {{--wire:click='create'--}}>
+            <i class="fas fa-shopping-cart"></i> Ir a ventas 
+          </a>
+          <a href="#" class="btn btn-danger btn-sm" wire:click='clear'>
+            <i class="fas fa-trash"></i> Cancelar Venta 
+          </a>
+       </x-slot>
+
+       {{-- Contenido --}}
+       <div class="row">
+            {{-- Detalles venta --}}
+            <div class="col-md-6">
+                @include('sales.card-details')
+            </div>
+            {{-- Detalles productos --}}
+            <div class="col-md-6">
+                @include('sales.list-products')
+            </div>
+       </div>
+
+       <x-slot:cardFooter>
+            
+       </x-slot>
+    </x-card>
+
 </div>
